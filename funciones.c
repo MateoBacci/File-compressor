917fac53e86b1c398bf4eeed20ad2599c5503354
MateoBacci <93776573+MateoBacci@users.noreply.github.com>
2022-06-02 22:44:32 -0300

    Update funciones.c

--- funciones.c
+++ funciones.c
@@ -46,10 +46,16 @@ void ordena_duplas(BTree* duplas, int inicio){ /* Selección */
   }
 }
 
-void duplas_destruir(BTree* duplas){
-  btree_destruir(*duplas);
-  free(duplas);
+char *str_copia(char *palabra, int len) {
+  char *palabraCopia = malloc((sizeof(char) * len) + 1);
+  for (int i = 0; i < len; i ++) {
+    palabraCopia[i] = palabra[i];
+  }
+  palabraCopia[len] = '\0';
+  return palabraCopia;
 }
+
+
 
 /**
  * chars[i] = codificacion en el arbol del caracter en la posicion i de la 
@@ -66,28 +72,98 @@ void char_codificacion (BTree arbol, char* strAux, char* arreglo[CANT_CHARS], in
     return;
   }
   if (es_hoja(arbol)) {
-    int pos = (unsigned char)arbol->caracter;
-    printf("%d %s\n", contador, strAux);
-    arreglo[pos] = malloc(contador + 1);
-    strncpy(arreglo[pos], strAux, contador);
+    unsigned char pos = (unsigned char)arbol->caracter;
+    arreglo[pos] = str_copia(strAux, contador);
   } else {
     strAux[contador] = '0';
     contador++;
   }
+  
   char_codificacion(arbol->left, strAux, arreglo, contador);
   strAux[contador-1] = '1';
   char_codificacion(arbol->right, strAux, arreglo, contador);
 }
 
 char* codificar_texto(char* buf, int len, char* codes[CANT_CHARS], int* nLen){
-  char* textoCodificado = malloc(1);
   int textLen = 0;
+  int capBuf = len * 8;
+  char* textoCodificado = malloc(capBuf+1);
+  textoCodificado[0] = '\0';
+  unsigned char pos;
   for (int i = 0; i < len; i++) {
-    int pos = (unsigned char)buf[i];
+    pos = (unsigned char)buf[i];
+    if (textLen >= capBuf) {
+      textoCodificado = realloc(textoCodificado, capBuf*2 + 1);
+      capBuf *= 2;
+    }
+    for (int j = 0; j < strlen(codes[pos]); j++)
+      textoCodificado[textLen + j] = codes[pos][j];
     textLen += strlen(codes[pos]);
-    textoCodificado = realloc(textoCodificado, textLen + 1);
-    textoCodificado = strcat(textoCodificado, codes[pos]);
+    textoCodificado[textLen] = '\0';
   }
   *nLen = textLen;
   return textoCodificado;
+}
+
+
+char buscar_caracter(BTree arbol, char *buf, int *pos) {
+  if (es_hoja(arbol)){
+    return arbol->caracter;
+  }
+  *pos += 1;
+
+  if (buf[*pos - 1] == '0') 
+    return buscar_caracter(arbol->left, buf, pos);
+  else 
+    return buscar_caracter(arbol->right, buf, pos);
+  
+
+}
+
+char *decodificar_texto(BTree arbol, char *buf, int len, int *i) {
+  int largo = len; 
+  char *texto = malloc(largo);
+  texto[0] = '\0';
+  for (int pos = 0; pos < len, *i <= pos; *i += 1) {
+    if (*i > largo) {
+      texto = realloc(texto, largo + len + 1);
+    }
+    texto[*i] = buscar_caracter(arbol, buf, &pos);
+    printf("%d/%d\t%ld\n", pos, largo, strlen(texto));
+  }
+  return texto;
+}
+
+BTree arbol_desde_texto (char *code, int *pos, int *charsAgregados) {
+  BTree arbol = malloc(sizeof(Nodo));
+  *pos += 1;
+  if (code[*pos - 1] == '1' && *pos < 512) {
+    arbol->caracter = code[*charsAgregados + 511];
+    arbol->left = arbol->right = NULL;
+    *charsAgregados += 1;
+  } else if (code[*pos - 1] == '0' && *pos < 512){
+    arbol->left = arbol_desde_texto(code, pos, charsAgregados);
+    arbol->right = arbol_desde_texto(code, pos, charsAgregados);
+  }
+  return arbol;
+}
+
+
+
+char *cambio_archivo (char *fileName, char *dato) {
+  int len = strlen(fileName) + strlen(dato);
+  char *newFileName = malloc(sizeof(char) * len);
+  for (int i = 0; i < len; i++) {
+    if (i < strlen(fileName) - 2)
+      newFileName[i] = fileName[i];
+    else
+      newFileName[i] = dato[i - strlen(fileName) + 2];
+  }
+  return newFileName;
+}
+
+
+void destruir_arreglo(char *chars[CANT_CHARS]){
+  for (int i = 0; i < CANT_CHARS; i++)
+      free(chars[i]);
 }
