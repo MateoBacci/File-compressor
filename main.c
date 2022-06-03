0255fcf6ff8682300d9740d82ccb716e08f73ae5
MateoBacci <93776573+MateoBacci@users.noreply.github.com>
2022-06-02 22:45:01 -0300

    Update main.c

--- main.c
+++ main.c
@@ -7,75 +7,86 @@
 #include "btree.h"
 #include "funciones.h"
 
-void mostrar_chars(char *chars[CANT_CHARS]){
-  for (int i = 0; i < CANT_CHARS; i++)
-    printf("%d) %s\n", i, chars[i]);
-}
-
 int main(int argI, char *argC[3]){
 
   if (argC[1][0] == 'C' || argC[1][0] == 'c') {
 
-    BTree* duplas;
-    char *buf, *fileName = argC[2];
+    char *fileName = argC[2];
     int bufLen;
-
-    buf = readfile(fileName, &bufLen);
-    duplas = crear_duplas();
+    char *buf = readfile(fileName, &bufLen);
+    BTree* duplas = crear_duplas();
     contar_caracteres(duplas, buf, bufLen);
     ordena_duplas(duplas, 0);
 
-
-
     BTree arbol = btree_armar(duplas);
-    char *codeArbol = malloc(CANT_CHARS*3);
+    free(duplas);
+    
+    
+    char *codeArbol = malloc(sizeof(char) * CANT_CHARS * 3);
     char *codeChars[CANT_CHARS];
-    char *aux = malloc(CANT_CHARS);
-    char *pathTree = strcat(fileName, ".tree");;
+    char *aux = malloc(CANT_CHARS * sizeof(char));
+    char *pathTree = malloc(sizeof(char) * strlen(fileName) + 6);
     int lenCode = 0, lenChars = 0;
+    strcpy(pathTree, fileName);
+    strcat(pathTree, ".tree");
 
     btree_codificacion(arbol, codeArbol, &lenCode, &lenChars);
     writefile(pathTree, codeArbol, (CANT_CHARS*3) - 1);
-    char_codificacion(arbol, aux, codeChars, 0);
-    mostrar_chars(codeChars);
-
-
-
-    char* bufCodificado;
-    int nLen;
-    char* pathHf = strcat(fileName, ".hf");
-  
-    bufCodificado = codificar_texto(buf, bufLen, codeChars, &nLen);
-    bufCodificado = implode(bufCodificado, strlen(bufCodificado), &nLen);
-    writefile(pathHf, bufCodificado, nLen);
-
+    free(pathTree);
     
-    /* Liberamos la memoria dinámica */
-    duplas_destruir(duplas);
-    free(buf);
-    for (int i = 0; i < CANT_CHARS; i++)
-      free(codeChars[i]);
+    char_codificacion(arbol, aux, codeChars, 0);
+    btree_destruir(arbol);
     free(codeArbol);
     free(aux);
 
 
+    int nLen;
+    char* pathHf = malloc(sizeof(char) * strlen(fileName) + 4);
+    strcpy(pathHf, fileName);
+    strcat(pathHf, ".hf");
+    char *bufCodificado = codificar_texto(buf, bufLen, codeChars, &nLen);
+    free(buf);
+    destruir_arreglo(codeChars);  
+
+    char *bufImplotado = implode(bufCodificado, strlen(bufCodificado), &nLen);
+    free(bufCodificado);
+
+    writefile(pathHf, bufImplotado, nLen);
+    free(bufImplotado);
+    free(pathHf);
+
+    printf("Listo!\n");
     return 0;
   }
   else if (argC[1][0] == 'D' || argC[1][0] == 'd') {
     char *fileName = argC[2];
-    char *buf;
-    int bufLen;
-    int bufELen;
-    buf = readfile(fileName, &bufLen);
+    int bufLen, bufELen;
+    char *buf = readfile(fileName, &bufLen);
+    char *bufExploded = explode(buf, bufLen, &bufELen);
+    free(buf);
+
+    char *pathTree = cambio_archivo(fileName, "tree");
+
+    int codeArbolLen, contA = 0, contB = 0;
+    char *codeArbol = readfile(pathTree, &codeArbolLen);;
+    BTree arbol = arbol_desde_texto(codeArbol, &contA, &contB);
+    free(pathTree);
+    free(codeArbol);
+
+    int lenDecoded = 0;
+    char *textoDecodificado;
+    textoDecodificado = decodificar_texto(arbol, bufExploded, bufELen, &lenDecoded);
+    free(bufExploded);
+    btree_destruir(arbol);
     
-
-
-
-
-    char *pathDec = strcat(fileName, ".dec");
-    //writefile(pathDec, , )
+    char *pathDec = cambio_archivo(fileName, "dec");
+    writefile(pathDec, textoDecodificado, lenDecoded);
+    free(pathDec);
+    free(textoDecodificado);
+    puts("Listo!");
     return 1;
   }
+
   perror("Caracter no aceptado, elija 'C' o 'D'");
   return 2;
 }
